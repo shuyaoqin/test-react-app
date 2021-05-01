65fda59869efdebb40d0f7a13b00461fcf775f2a
shu <1500893898@qq.com>
2021-05-01 17:16:05 +0800

    仿天猫轮播图的实现

---
+++ my-app/src/components/header/index.js
@@ -0,0 +1,11 @@
+import React, {Component} from  'react';
+class HeaderComponent extends Component {
+    render() {
+        return (
+            <div id="header">
+                头部
+            </div>
+        )
+    }
+}
+export default HeaderComponent;
